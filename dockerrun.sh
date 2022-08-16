cda1911bddcc8ba979a8a80e649d1be3d7b22902
cybe <47478929+cybe42@users.noreply.github.com>
2022-08-16 17:40:06 +0100

    add a script to run docker

---
+++ dockerrun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo docker run -it --rm cybe42/subnukebutitworks # run the docker
+#I'll assume you got sudo here but if you don't got an alias for it you should be taking the time to add some alias to your shell config.
